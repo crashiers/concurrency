41e204328a999860b2f2c635dc306958885c3271
xjn <HelloMrXu@foxmail.com>
2018-06-19 22:38:29 +0800

    CAS

---
+++ src/main/java/com/mmall/concurrency/example/atomic/AtomicExample3.java
@@ -0,0 +1,60 @@
+package com.mmall.concurrency.example.atomic;
+
+import com.mmall.concurrency.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.stream.Stream;
+
+/**
+ * 线程安全:
+ * 	不管运行时采用何种调度方式,并且在主调代码中不需要额外的协同或者同步,都能表现出正确的行为
+ */
+@Slf4j
+@ThreadSafe
+public class AtomicExample3 {
+	//请求总数
+	public static int clientTotal = 5000;
+
+	public static int threadTotal = 200;
+
+	/**
+	 * LongAdder - AtomicLong
+	 * ceil     -  do-while
+	 */
+	public static LongAdder count = new LongAdder();
+
+	public static void main(String[] args) throws InterruptedException {
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		//信号量
+		final Semaphore semaphore = new Semaphore(threadTotal);
+		//计数器闭锁
+		final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+		Stream.iterate(0,n->n+1)
+				.limit(clientTotal)
+				.forEach(e->{
+					executorService.execute(()->{
+						try {
+							semaphore.acquire();
+							add();
+							semaphore.release();
+						} catch (Exception e1) {
+							log.error("exception",e);
+						}
+						countDownLatch.countDown();
+					});
+				});
+		countDownLatch.await();
+		executorService.shutdown();
+		log.info("count:{}",count);
+	}
+	public static void add(){
+		count.increment();
+	}
+}
+
---
+++ src/main/java/com/mmall/concurrency/example/atomic/AtomicExample4.java
@@ -0,0 +1,34 @@
+package com.mmall.concurrency.example.atomic;
+
+import com.mmall.concurrency.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.stream.Stream;
+
+/**
+ * 线程安全:
+ * 	不管运行时采用何种调度方式,并且在主调代码中不需要额外的协同或者同步,都能表现出正确的行为
+ */
+@Slf4j
+@ThreadSafe
+public class AtomicExample4 {
+	private static AtomicReference<Integer> count = new AtomicReference<>(0);
+	public static void main(String[] args) {
+		count.compareAndSet(0,2);
+		count.compareAndSet(0,1);
+		count.compareAndSet(2,4);
+		count.compareAndSet(6,1);
+		count.compareAndSet(5,2);
+		count.compareAndSet(4,3);
+		count.compareAndSet(2,42);
+		count.compareAndSet(2,2);
+		log.info("count:{}",count.get());
+	}
+}
+
